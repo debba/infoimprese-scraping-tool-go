04a81b0e5d50d8298192ac71daf7e0f19081c224
debba <andrea@debbaweb.it>
2019-09-27 13:50:38 +0200

    add captcha balance in log

--- decrypt/decrypt.go
+++ decrypt/decrypt.go
@@ -3,6 +3,7 @@ package decrypt
 import (
 	"github.com/debba/anticaptcha"
 	"log"
+	"os"
 	"time"
 )
 
@@ -10,6 +11,12 @@ func GetCaptcha(apiKey string, siteKey string, url string) (string, error) {
 	log.Printf("[CHECKING] captcha (url = %s)", url)
 	log.Printf("[CHECKING] captcha (siteKey = %s, apiKey = %s)", siteKey, apiKey)
 	client := anticaptcha.Client{APIKey: apiKey}
+	balance, berr := client.GetBalance()
+	if berr != nil {
+		log.Printf("[ERROR] error = %s", berr.Error())
+		os.Exit(0)
+	}
+	log.Printf("[ANTICAPTCHA BALANCE] Current: %2f", balance)
 	captcha, err := client.SendRecaptcha(
 		url,
 		siteKey,
@@ -17,6 +24,7 @@ func GetCaptcha(apiKey string, siteKey string, url string) (string, error) {
 
 	if err != nil {
 		log.Printf("[ERROR] error = %s", err.Error())
+		os.Exit(0)
 	}
 
 	return captcha, err
