ada829c44c652559c687786bcaf7e5b8d096f184
debba <andrea@debbaweb.it>
2019-09-28 15:43:38 +0200

    windows batch fixes

--- exec.bat
+++ exec.bat
@@ -3,9 +3,9 @@ rem For you fucking Windows User
 set /P query="What are you looking for? "
 set /P where="Where? "
 set /P filename="Filename "
-set /P mode="Mode "
+set /P mmode="Mode "
 
 IF NOT DEFINED filename SET "filename=export"
-IF NOT DEFINED mode SET "mode=search_by_name"
+IF NOT DEFINED mmode SET "mmode=with_email"
 
-infoimprese.exe -q %query% -l %where% -m %mode% -o risultati/%filename%.csv
+infoimprese.exe -q %query% -l %where% -m %mmode% -o risultati/%filename%.csv
